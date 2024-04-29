2893c8b62622ec83dd54163293f87e62d352b946
ytyng <ytyng@live.jp>
2024-04-29 09:56:20 +0900

    ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   css: [
     '~/assets/css/site.css'
   ],
